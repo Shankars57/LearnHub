33a7c81c99776779d4a32347ff8da228bad675e8
MadCoder7 <shankarpubg4@gmail.com>
2025-11-25 12:39:37 +0530

    Updates

--- frontend/store/useMaterialStore.js
+++ frontend/store/useMaterialStore.js
@@ -2,11 +2,13 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 const useMaterialStore = create(
-  persist((set) => ({
-    options: "mat",
-    setOptions: (state) => set({ options: state }),
-  })),
-  { name: "materials" }
+  persist(
+    (set) => ({
+      options: "mat",
+      setOptions: (state) => set({ options: state }),
+    }),
+    { name: "materials" }
+  )
 );
 
 export default useMaterialStore;
